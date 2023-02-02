490674794f341114e0affef8029147dc56cee061
aleksst85 <65343095+aleksst85@users.noreply.github.com>
2023-02-02 08:24:31 +0700

    1

--- config.py
+++ config.py
@@ -1,4 +1,2 @@
 JSON_PATCH = 'data.json'
 
-from util import load_json
-print(load_json(JSON_PATCH))
--- util.py
+++ util.py
@@ -1,24 +1,43 @@
+
 import json
+from config import JSON_PATCH
+
 def game_space(questions):
     """Создаем и модифицируем  игровое пространство"""
-    for quest in questions:
-        price_list = []
-        for i in questions[quest]:
-            for j in questions[quest][i]:
-               position = questions[quest][i]['asked']
-            if not position:
-                price_list.append(i)
+    for category_name, category_questions in questions.items():
+        print(category_name.ljust(17), end="")
+        for price, question_data in category_questions.items():
+            asked = question_data['asked']
+            if not asked:
+                print(str(price).ljust(5), end="")
             else:
-                price_list.append("...")
-        print(f"{quest}{(15-len(quest))*' '}{'   '.join(price_list)}")
+                print("   ".ljust(5), end = "")
+        print()
+
+
+#   мой вариант
+#        price_list = []
+#        for i in questions[quest]:
+#            for j in questions[quest][i]:
+#               position = questions[quest][i]['asked']
+#            if not position:
+#                price_list.append(i)
+#            else:
+#                price_list.append("...")
+#        print(f"{quest}{(15-len(quest))*' '}{'   '.join(price_list)}")
+        
 
 
 def parse_input(quest_num):
     category, count = quest_num.split(" ")
     return category, count
 
+
 def load_json(patch):
     with open(patch, 'r') as file:
         data_json = file.read()
     questions = json.loads(data_json)
     return questions
+
+
+
